eabdc3059a95ca83709746ee89bc153e447a7715
lightkun10 <lifeisubw@gmail.com>
2020-11-30 08:43:48 +0700

    Complete main features(appbar, jumbotron, resto lists, footer)

--- src/scripts/doms.js
+++ src/scripts/doms.js
@@ -1,38 +1,38 @@
 import data from "../DATA.json";
 
-/*
-<article class="restaurant-item">
-    <img class="restaurant-item__thumbnail" 
-        src="https://dicoding-restaurant-api.el.r.appspot.com/images/medium/07" 
-        alt="restaurant picture">
-    <div class="restaurant-item__content">
-        <h1 class="resturant-item__name">Run The Gun</h1>
-        <p class="restaurant-item__description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet.</p>
-        <p class="resturant-item__city">Bali</p>
-        <p class="resturant-item__rating">4.6</p>
-    </div>
-</article>
-*/
-
 const Doms = (function() {
     // console.log("Hello from doms!")
 
     const _restaurantsSection = document.querySelector(".restaurants");
 
-    console.log(data.restaurants);
-
     const displayRestaurants = () => {
         data.restaurants.forEach(restaurant => {
-            let article = document.createElement("article");
-            article.className = "restaurant-item";
-                let artiImg = document.createElement("img");
-                artiImg.className = "restaurant-item__thumbnail";
-                artiImg.src = `${restaurant['pictureId']}`;
+            let restoItem = document.createElement("article");
+            restoItem.className = "restaurant-item";
+                let restoImg = document.createElement("img");
+                restoImg.className = "restaurant-item__thumbnail";
+                restoImg.src = `${restaurant['pictureId']}`;
+                restoImg.setAttribute("alt", "restaurant picture");
                 
+                let restoCont = document.createElement("div");
+                restoCont.className = "restaurant-item__content";
+                    let restoName = document.createElement("h1");
+                    restoName.className = "restaurant-item__name";
+                    restoName.textContent = restaurant["name"];
+                    let restoCity = document.createElement("p");
+                    restoCity.className = "restaurant-item__city";
+                    restoCity.textContent = restaurant["city"];
+                    let restoDesc = document.createElement("p");
+                    restoDesc.className = "restaurant-item__description";
+                    restoDesc.textContent = restaurant["description"];
+                    let restoRating = document.createElement("p");
+                    restoRating.className = "restaurant-item__rating";
+                    restoRating.textContent = `${restaurant["rating"]} / 5`;
+                restoCont.append(restoName, restoCity, restoDesc, restoRating);
 
-                article.append(artiImg);
+                restoItem.append(restoImg, restoCont);
     
-            _restaurantsSection.appendChild(article);
+            _restaurantsSection.appendChild(restoItem);
         })
     }
     
