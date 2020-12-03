015692a549b2c4c695aede32f3514e85c941b3ae
lightkun10 <lifeisubw@gmail.com>
2020-12-03 18:24:15 +0700

    Appbar for pwa

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,9 +2,9 @@ import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 import '../styles/responsive.css';
 import '../styles/skip-link.css';
-import HamburgerBtn from '../modules/hamburgerBtn';
+// import HamburgerBtn from '../modules/hamburgerBtn';
 import Doms from '../modules/doms';
 
-HamburgerBtn.btnSet();
+// HamburgerBtn.btnSet();
 
 Doms.displayRestaurants();
