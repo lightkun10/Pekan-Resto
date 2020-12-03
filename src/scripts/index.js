fa1eb702ca2750d06a82ade3b0403853f1f90777
lightkun10 <lifeisubw@gmail.com>
2020-12-03 18:36:47 +0700

    Navigation drawer config for pwa

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,7 +4,14 @@ import '../styles/responsive.css';
 import '../styles/skip-link.css';
 // import HamburgerBtn from '../modules/hamburgerBtn';
 import Doms from '../modules/doms';
+import App from './views/app';
 
 // HamburgerBtn.btnSet();
 
 Doms.displayRestaurants();
+
+const app = new App({
+  button: document.querySelector('#hamburger'),
+  drawer: document.querySelector('#drawer'),
+  content: document.querySelector('#mainContent'),
+});
