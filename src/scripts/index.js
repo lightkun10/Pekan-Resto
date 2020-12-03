35b9cf64f7c669f38d4eeb0db320763353fc0b43
lightkun10 <lifeisubw@gmail.com>
2020-12-03 13:15:47 +0700

    Move module to its own dir

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,8 +2,8 @@ import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 import '../styles/responsive.css';
 import '../styles/skip-link.css';
-import HamburgerBtn from './hamburgerBtn';
-import Doms from './doms';
+import HamburgerBtn from '../modules/hamburgerBtn';
+import Doms from '../modules/doms';
 
 HamburgerBtn.btnSet();
 
